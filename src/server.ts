9a6009bfc07c7f30d184454e49aa128dd049225a
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-07 20:44:03 -0300

    feat: project base structure

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import * as dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
