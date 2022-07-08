f4cc5ca90b624d8a5a3f789555352af5663e7f2d
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-08 16:11:31 -0300

    feat: using express.json and import categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import * as dotenv from "dotenv";
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
