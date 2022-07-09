d902076274e2bb64aabb7bcc5b6935d2f8f83b36
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-09 19:05:09 -0300

    feat: CreateCategoryService

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,27 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+    /* Aplicando o DIP (Dependency Inversion Principle) do princípio SOLID (categoriesRepository no constructor).
+    Invés do serviço ter essa dependência), vamos deixar essa responsabilidade para quem chamar o service.
+    Se essa responsabilidade fosse do service, sempre teríamos uma instância diferente de uma categoria quando
+    fossemos criar nos métodos */
+    constructor(private categoriesRepository: CategoriesRepository) {}
+
+    execute({ description, name }: IRequest): void {
+        const categoryAlreadyExists =
+            this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error("Category already exists!");
+        }
+
+        this.categoriesRepository.create({ description, name });
+    }
+}
+
+export { CreateCategoryService };
