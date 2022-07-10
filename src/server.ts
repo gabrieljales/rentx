4ed69d95fbf1e9d5a6d0b150addf428d5d0cda37
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-10 16:53:37 -0300

    feat: specifications post route created

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
