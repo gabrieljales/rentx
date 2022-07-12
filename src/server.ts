c1a64548b2d8c68606ab7bb55fb6bbc741fd7fa0
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-11 21:13:06 -0300

    refactor: index file to centralize all routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import * as dotenv from "dotenv";
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
