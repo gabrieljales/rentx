379105149489d3b7d63563af95e3d53df888979c
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-12 21:08:06 -0300

    docs: initial documentation with swagger-ui-express

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
