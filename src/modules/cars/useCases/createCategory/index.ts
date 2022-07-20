e6b85b8be5b164b980cb3035be57ab3350b6151d
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-20 20:33:10 -0300

    refactor: add type

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -2,7 +2,8 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-export default () => {
+// Envolvendo em uma função, nos permite ter um controle maior. A função só vai ser acessada, quando for chamada
+export default (): CreateCategoryController => {
   // Instanciando o CategoriesRepository (singleton)
   const categoriesRepository = new CategoriesRepository();
 
