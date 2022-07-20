c8c633387cf86da2c8450ef87dbd794e856f5561
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-20 20:43:24 -0300

    refactor: add comments

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -3,6 +3,8 @@ import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 // Envolvendo em uma função, nos permite ter um controle maior. A função só vai ser acessada, quando for chamada
+// Logo, o repositório só será instanciado quando a função for chamada (além de que só chama depois que a conexão com o
+// bd foi feita)
 export default (): CreateCategoryController => {
   // Instanciando o CategoriesRepository (singleton)
   const categoriesRepository = new CategoriesRepository();
