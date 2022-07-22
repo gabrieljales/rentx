7085d0cbc8cdb0aba75c45aebe1db7089217f0d8
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-21 21:08:49 -0300

    feat: SpecficationRepository container created

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,9 +3,16 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   // Nome do container e a classe que queremos chamar toda vez que o nome for chamado
   "CategoriesRepository",
   CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
 );
