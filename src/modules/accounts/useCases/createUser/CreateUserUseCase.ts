30944f9930abb820a7a29c5fd290e9902bd7d888
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-23 19:44:34 -0300

    feat: CreateUser UseCase (initial)

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,29 @@
+import { inject } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    drive_license,
+    email,
+    name,
+    password,
+    username,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      drive_license,
+      email,
+      name,
+      password,
+      username,
+    });
+  }
+}
+
+export { CreateUserUseCase };
