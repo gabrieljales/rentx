46a6ba035c183ae0de273dd668714357edce1b42
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-07-23 19:30:25 -0300

    feat: Users Repository

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,34 @@
+import { Repository } from "typeorm";
+
+import { PostgresDataSource } from "../../../../database";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = PostgresDataSource.getRepository(User);
+  }
+
+  async create({
+    name,
+    username,
+    email,
+    drive_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      drive_license,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
