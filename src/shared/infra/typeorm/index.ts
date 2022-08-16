183eb083da2c25d5022f1ec1adff9ff450494815
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-08-16 11:06:26 -0300

    refactor: Change migrations and entities paths

---
+++ src/shared/infra/typeorm/index.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import * as dotenv from "dotenv";
+import { join } from "path";
+import { DataSource } from "typeorm";
+
+dotenv.config();
+
+const PostgresDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  migrations: [join(__dirname, "../typeorm/migrations/*.{js,ts}")],
+  migrationsTableName: "migrations",
+  entities: [
+    join(__dirname, "../../../modules/**/infra/typeorm/entities/*.{ts,js}"),
+  ],
+});
+
+export { PostgresDataSource };
