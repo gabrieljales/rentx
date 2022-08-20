42e4b11dfc474b0e4ef8a5d48226f31d06ba209c
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-08-19 21:04:18 -0300

    refactor: Update Car entity with Typeorm

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,32 +1,54 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+import { Category } from "./Category";
+
+@Entity("cars")
 class Car {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
+  @Column({ default: true })
   available: boolean;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category;
+
+  @Column()
   brand: string;
 
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
-  // Será substituído pelo typeorm
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
-      this.available = true;
-      this.created_at = new Date();
     }
   }
 }
