0344597c7a7fda0047ea46cbe0bf6df8b922f573
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-09-06 22:33:59 -0300

    refactor: ListAvailableCars route added

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const carsRoutes: Router = Router();
 
 const createCarController: CreateCarController = new CreateCarController();
+const listAvailableCarsController: ListAvailableCarsController =
+  new ListAvailableCarsController();
 
 carsRoutes.post(
   "/",
@@ -14,5 +17,7 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
