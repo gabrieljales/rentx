8d520ed8fa08763471e9cc3400a60b4fcc4d53ae
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-09-26 16:10:56 -0300

    refactor: Configs for test database added
    If NODE_ENV=test, use test database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -7,11 +7,14 @@ dotenv.config();
 
 const PostgresDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
+  host: process.env.NODE_ENV === "test" ? "localhost" : process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_DB_NAME
+      : process.env.DB_NAME,
   migrations: [join(__dirname, "../typeorm/migrations/*.{js,ts}")],
   migrationsTableName: "migrations",
   entities: [
