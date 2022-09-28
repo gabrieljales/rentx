6448c0399c3e43e517f8c1be90bae1c87a2a92ec
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-09-28 14:21:32 -0300

    feat: DevolutionRental route created

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -8,7 +9,14 @@ const rentalsRoutes: Router = Router();
 
 const createRentalController: CreateRentalController =
   new CreateRentalController();
+const devolutionRentalController: DevolutionRentalController =
+  new DevolutionRentalController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalsRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export { rentalsRoutes };
